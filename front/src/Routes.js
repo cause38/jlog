c7f4249c6462e1395704a7a86b2b45d202129206
jj Park <jj.park@weballin.com>
2023-02-07 16:08:02 +0900

    list pages 삭제

--- front/src/Routes.js
+++ front/src/Routes.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import List from './pages/List';
-import Login from './pages/User/Login';
-import Logout from './pages/User/Logout';
-import './styles/css/main.css';
+import Layout from 'components/Layout';
+import Home from 'pages/Home';
+import Login from 'pages/User/Login';
+import Logout from 'pages/User/Logout';
+import 'styles/css/main.css';
 
 const Router = () => {
   return (
@@ -13,7 +12,6 @@ const Router = () => {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/list" element={<List />} />
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />
         </Routes>
