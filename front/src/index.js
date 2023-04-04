e0dfcdd7ba9b8f93d0b2f1dbe334aeb8b1a9370f
JJ Park <jj.park@weballin.com>
2023-04-04 09:27:34 +0900

    Build: Modify BE server PORT

--- front/src/index.js
+++ front/src/index.js
@@ -4,7 +4,7 @@ import Router from './Routes';
 import {RecoilRoot} from 'recoil';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.defaults.baseURL = 'http://localhost:8080/api';
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
